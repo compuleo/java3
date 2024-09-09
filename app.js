b0a14ef403f07ece807e3470092b7949cfab699a
Leonardo Aldana <112524903+compuleo@users.noreply.github.com>
2024-09-09 07:56:53 -0500

    A darle atomos

--- app.js
+++ app.js
@@ -7,5 +7,13 @@ let numero1 = 45;
 let numero2 = 15;
 console.log(numero1);
 
+// Operaciones basicas munecura
+
 let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
 console.log(suma);
+
+console.log(`LA SUMA DE ${numero1} Y ${numero2} ES ${suma}`);
+console.log(`LA RESTA DE ${numero1} Y ${numero2} ES ${resta}`);
+console.log(`LA MULTIPLICACION DE ${numero1} Y ${numero2} ES ${multiplicacion}`);
