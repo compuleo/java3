33c21b2fd60307f2f5d2463485d9a9b5e7f43ded
Leonardo Aldana <112524903+compuleo@users.noreply.github.com>
2024-09-10 07:58:10 -0500

    operadores logicos

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // Empezamos yehaaa!
 console.log("Hola Mundo");
 
-// variables
+// variables numericas
 
 let numero1 = 45;
 let numero2 = 15;
+let numero3 = 66;
+
+// variables boleanas
+
+let estado = true;
+
+// variables string
+
+let adress = "Cra 96A No 73 - 39 Alamos Norte";
+
 console.log(numero1);
 
 // Operaciones basicas munecura
@@ -12,8 +22,97 @@ console.log(numero1);
 let suma = numero1 + numero2;
 let resta = numero1 - numero2;
 let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+let mod = numero1 % numero2;
 console.log(suma);
 
 console.log(`LA SUMA DE ${numero1} Y ${numero2} ES ${suma}`);
 console.log(`LA RESTA DE ${numero1} Y ${numero2} ES ${resta}`);
 console.log(`LA MULTIPLICACION DE ${numero1} Y ${numero2} ES ${multiplicacion}`);
+console.log(`LA DIVISION DE ${numero1} Y ${numero2} ES ${division}`);
+console.log(`LA DIVISION MODULO DE ${numero1} Y ${numero2} ES ${mod}`);
+
+
+// Ingreso de data por el usuario
+
+//let nu1 = parseInt(prompt (("Ingrese el primer valor")));
+//let nu2 = parseInt(prompt (("Ingrese el segundo valor")));
+//let s = nu1 + nu2;
+
+//console.log(`EL PRIMER VALOR ES ${nu1} Y EL SEGUNDO ES ${nu2}`);
+//console.log(`LA SUMA ES ${s}`);
+
+
+// Nueros aleatorios
+
+//let alea = Math.floor(Math.random() * 50 ) + 1;
+//console.log(alea);
+
+
+/*
+// Estructura if
+
+
+let edad = 14;
+
+if (edad >= 18){
+    console.log("MAYOR DE EDAD");
+}else{
+    console.log("MENOR DE EDAD");
+}
+*/
+
+
+//EJERCICIO 2 LA CALIFICACION
+/*
+let calificacion;
+
+calificacion = parseFloat(prompt("ingrese su calificacion parserito"));
+
+if(calificacion < 3.0){
+    console.log("SE RAJO CHINO");
+}else{
+    console.log("PASO MANO");
+}
+*/
+
+//EJERCICIO 3 LA CALIFICACION
+/*
+let calificacion;
+
+calificacion = parseFloat(prompt("ingrese su calificacion parserito"));
+
+if(calificacion < 3.0){
+    console.log("SE RAJO CHINO");
+}else if(calificacion > 3.1 && calificacion <= 4.0){
+    console.log("PASO MANO");
+}else{
+    console.log("USTED ES MUY BUENO");
+}
+    */
+
+//QUE GENERACION ERES
+
+let generacion;
+
+generacion = parseFloat(prompt("EN QUE ANO NACIO PARCERITO?"));
+
+if(generacion <= 1900){
+    console.log("ERES DE LA GENERACION PERDIDA...");
+}else if(generacion >= 1901 && generacion <= 1924) {
+    console.log("ERES DE LA GENERACION GRANDIOSA...");
+}else if(generacion >= 1925 && generacion <= 1945) {
+    console.log("ERES DE LA GENERACION SILENCIOSA...");
+}else if(generacion >= 1946 && generacion <= 1964) {
+    console.log("ERES DE LA GENERACION BABYBOOMERS...");
+}else if(generacion >= 1965 && generacion <= 1980) {
+    console.log("ERES DE LA GENERACION X...");
+}else if(generacion >= 1981 && generacion <= 1996) {
+    console.log("ERES DE LA GENERACION Y...");
+}else if(generacion >= 1997 && generacion <= 2012) {
+    console.log("ERES DE LA GENERACION Z...");
+}else if(generacion >= 2013 && generacion <= 2024) {
+    console.log("ERES DE LA GENERACION ALPHA...");
+}else{
+    console.log("NO CORRESPONDE...");
+}
